93d1d23e34bff022cf92b56305086e928e426ffc
Gengaraj Subbiah <gangaraj1985@gmail.com>
2018-05-23 11:33:23 -0500

    Adding Logging Aspect

---
+++ src/main/java/com/tgt/retail/aspect/LoggingAspect.java
@@ -0,0 +1,45 @@
+package com.tgt.retail.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+	Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+	@Around("execution(* com.tgt.retail..*.*(..))")
+	public Object logTimeMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		Object retVal = joinPoint.proceed();
+		stopWatch.stop();
+
+		StringBuilder logMessage = new StringBuilder();
+		logMessage.append(joinPoint.getTarget().getClass().getName());
+		logMessage.append("::");
+		logMessage.append(joinPoint.getSignature().getName());
+		logMessage.append("(");
+		Object[] args = joinPoint.getArgs();
+		for (int i = 0; i < args.length; i++) {
+			logMessage.append(args[i]).append(",");
+		}
+		if (args.length > 0) {
+			logMessage.deleteCharAt(logMessage.length() - 1);
+		}
+		logMessage.append(")");
+		logMessage.append(" execution time :: ");
+		logMessage.append(stopWatch.getTotalTimeMillis());
+		logMessage.append(" ms");
+		logger.info(logMessage.toString());
+		return retVal;
+	}
+
+}
